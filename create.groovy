4f1ec783826d5d59efcedae12ca6ace8d891d04b
Karl Heinz Marbaise <info@soebes.de>
2014-02-27 20:37:02 +0100

    - try to use Maven 3.1.1.

--- create.groovy
+++ create.groovy
@@ -1,12 +1,20 @@
 job {
     name 'DSL-Tutorial-1-Test-Artifactor-maven-plugin'
+
     scm {
         git('git@github.com:khmarbaise/artifactor-maven-plugin.git')
     }
+
     triggers {
         scm('H/15 * * * *')
     }
+
     steps {
+/*        maven {
+            name = "Maven 3.1.1"
+        }
+*
+        mavenInstallation("Maven 3.1.1")
         maven('-B -Prun-its clean verify')
     }
 }
