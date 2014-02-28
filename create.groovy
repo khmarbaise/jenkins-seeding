93b616465844b2cc7c0ba7e11fd094bf9c5116e9
Karl Heinz Marbaise <info@soebes.de>
2014-02-28 14:26:10 +0100

    - Added.

--- create.groovy
+++ create.groovy
@@ -1,6 +1,31 @@
+import hudson.scm.*
+import hudson.model.*
+import org.tmatesoft.svn.core.*
+import org.tmatesoft.svn.core.io.*
+import java.util.*
+
 // job (type: Maven) {
 
 def existingMavenInstallations = [ "Maven 2.0.11", "Maven 2.2.1", "Maven 3.0.5", "Maven 3.1.0", "Maven 3.1.1" ]
+
+def svn_root = 'http://svn.apache.org/repos/asf/maven/plugins/'
+
+def modules() {
+  ret = []
+
+  def repository = new SubversionSCM.ModuleLocation(svn_root, '.').openRepository(template)
+  for(entry in repository.getDir('trunk', SVNRepository.INVALID_REVISION, (SVNProperties) null, (Collection) null)) {
+    ret.add(entry.name)
+  }
+
+  ret
+}
+
+def modules = modules()
+
+modules.each {
+  println "Module:" + it
+}
 
 job {
 
